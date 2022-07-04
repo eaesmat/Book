760eaea5305994e7bc75d689baa5e83724fb5b41
Esmatullah Ahmadzai <ea.ahmadzai2020@gmail.com>
2022-07-04 18:49:16 +0430

    Add remove option

--- index.js
+++ index.js
@@ -1,114 +1,102 @@
 var form = `<div>
   <div class="form-group">
-    <label for="name">Name</label>
-    <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter Your Name">
+    <label for="title">Title</label>
+    <input type="text" class="form-control" id="title" aria-describedby="authorHelp" placeholder="Enter Your title">
   </div>
   <div class="form-group mt-3">
-    <label for="email">Email</label>
-    <input type="email" class="form-control" id="email" placeholder="Enter Your email">
+    <label for="author">Author</label>
+    <input type="author" class="form-control" id="author" placeholder="Enter Your author">
   </div>
-  <button type="submit" class="btn btn-primary mt-3" onclick="save()">Save</button>
+  <button type="submit" class="btn btn-primary mt-3" onclick="ADD()">ADD</button>
 </div>`;
 
 function table() {
-    let table = `<table class="table">
+  let table = `<table class="table">
   <thead>
     <tr>
       <th clsaa="col-1">NO</th>
-      <th clsaa="col-3">Name</th>
-      <th clsaa="col-4">Email</th>
-      <th clsaa="col-2">Edit</th>
-      <th clsaa="col-2">Delete</th>
+      <th clsaa="col-3">title</th>
+      <th clsaa="col-4">Author</th>
+      <th clsaa="col-2">Remove</th>
     </tr>
   </thead>
   <tbody>`;
-    for (let i = 0; i < details.length; i++){
-        table = table + `<tr>
+  for (let i = 0; i < details.length; i++) {
+    table =
+      table +
+      `<tr>
       <td>${i + 1}</td>
-      <td>${details[i].name}</td>
-      <td>${details[i].email}</td>
-      <td><button type="button" class="btn btn-warning" onclick="edit(${i})">Edit</button></td>
-      <td><button type="button" class="btn btn-danger" onclick="deleteData(${i})">Delete</button></td>
+      <td>${details[i].title}</td>
+      <td>${details[i].author}</td>
+      <td><button type="button" class="btn btn-danger" onclick="RemoveData(${i})">Remove</button></td>
     </tr> `;
-    };
-    table = table+`</tbody>
+  }
+  table =
+    table +
+    `</tbody>
     </table>`;
-    document.getElementById("table").innerHTML = table;
-};
+  document.getElementById("table").innerHTML = table;
+}
 document.getElementById("form").innerHTML = form;
 details = [];
 getData();
 table();
-function getData(){
-    let Data = localStorage.getItem("details");
-    if (Data) {
-        details = JSON.parse(Data);
-    } else {
-        setData();
-    };
-};
+function getData() {
+  let Data = localStorage.getItem("details");
+  if (Data) {
+    details = JSON.parse(Data);
+  } else {
+    setData();
+  }
+}
 function setData() {
-    localStorage.setItem("details", JSON.stringify(details));
-};
-function save() {
-    let name = document.getElementById("name");
-    let email = document.getElementById("email");
+  localStorage.setItem("details", JSON.stringify(details));
+}
+function ADD() {
+  let title = document.getElementById("title");
+  let author = document.getElementById("author");
 
-    if (name.value == 0) {
-        alert("name is Empty");
-        return
-    }
-    let data = {
-        name: name.value,
-        email: email.value
-    };
-    details.push(data);
-    setData();
+  if (title.value == 0) {
+    alert("title is Empty");
+    return;
+  }
+  if (author.value == 0) {
+    alert("Author is Empty");
+    return;
+  }
+  let data = {
+    title: title.value,
+    author: author.value,
+  };
+  details.push(data);
+  setData();
 
-    // console.log(details)
-    // console.log(email.value)
-    table();
-    name.value = "";
-    email.value = "";
-};
-function deleteData(index) {
-    details.splice(index, 1);
-    setData();
-    table();
+  // console.log(details)
+  // console.log(author.value)
+  table();
+  title.value = "";
+  author.value = "";
+}
+function RemoveData(index) {
+  details.splice(index, 1);
+  setData();
+  table();
 
-    // console.log('delete work')
-    // console.log(details)
-};
-
-function edit(index) {
-    let editForm = `<div>
-  <div class="form-group">
-    <label for="name">Update Name</label>
-    <input type="text" value="${details[index].name}" class="form-control" id="newName" aria-describedby="emailHelp" placeholder="Update Your Name">
-  </div>
-  <div class="form-group mt-3">
-    <label for="email">Email</label>
-    <input type="email" value="${details[index].email}" class="form-control" id="newEmail" placeholder="Update Your email">
-  </div>
-  <button type="submit" class="btn btn-primary mt-3" onclick="update(${index})">Update</button>
-</div>`;
-    document.getElementById("form").innerHTML = editForm;
-    // console.log('edit work');
-};
-function update(index) {
-    let newName = document.getElementById('newName');
-    let newEmail = document.getElementById('newEmail');
-
-    details[index] = {
-        name: newName.value,
-        email: newEmail.value
-    };
-    setData();
-    table();
-    document.getElementById("form").innerHTML = form;
-// console.log('update work')
-// console.log(details)
+  // console.log('Remove work')
+  // console.log(details)
 }
 
+function update(index) {
+  let newtitle = document.getElementById("newtitle");
+  let newauthor = document.getElementById("newauthor");
 
-
+  details[index] = {
+    title: newtitle.value,
+    author: newauthor.value,
+  };
+  setData();
+  table();
+  document.getElementById("form").innerHTML = form;
+  // console.log('update work')
+  // console.log(details)
+}
