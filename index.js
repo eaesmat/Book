48f08b36eefc05a6261b30c38bbe0913c2f9d35d
Esmatullah Ahmadzai <ea.ahmadzai2020@gmail.com>
2022-07-05 00:44:35 +0430

    Fix linters

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+function getData() {
+  const Data = localStorage.getItem('details');
+  if (Data) {
+    details = JSON.parse(Data);
+  } else {
+    setData();
+  }
+}
+
+function ADD() {
+  const title = document.getElementById('title');
+  const author = document.getElementById('author');
+  const data = {
+    title: title.value,
+    author: author.value,
+  };
+  details.push(data);
+  setData();
+
+  // console.log(details)
+  // console.log(author.value)
+  table();
+  title.value = '';
+  author.value = '';
+}
+function RemoveData(index) {
+  details.splice(index, 1);
+  setData();
+  table();
+
+  // console.log('Remove work')
+  // console.log(details)
+}
+
 /* eslint linebreak-style: ["error", "unix"] */
 const form = `<div>
   <div class="form-group">
@@ -26,7 +60,7 @@ function table() {
       <td>${i + 1}</td>
       <td>${details[i].title}</td>
       <td>${details[i].author}</td>
-      <td><button type="button" class="btn btn-danger" onclick="RemoveData(${i})">Remove</button></td>
+      <td><button type="button" class="btn btn-danger" id="remove" onclick="RemoveData(${i})">Remove</button></td>
     </tr> `;
   }
   table += `</tbody>
